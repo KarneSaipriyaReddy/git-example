458bd4caf1e67d81a60ad4591fc4450b4de8f1eb
saipriya <karne.saipriyareddy@gmail.com>
2023-07-27 18:06:18 +0530

    I learnt short cuts today���

---
+++ script.py
@@ -0,0 +1,13 @@
+name="sai priya"
+# print("persons name=", name)
+
+city_name="hyderabad"
+# print(city_name)
+
+# INDIA IS MY COUNTRY 
+
+print("2 * 1 = 2")
+print("2 * 2 = 4")
+print("2 * 3 = 6")
+print("2 * 4 = 8")
+print("2 * 5 = 10")
